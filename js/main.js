b4677ff5087f3e29985e2bd6cb104c3f12b04163
Your Name <you@example.com>
2021-11-23 15:40:26 +0500

    Loader

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+$('document').ready(
+    setTimeout(function () {
+        $('.loader').css('opacity', '0');
+        setTimeout(function () {
+            $('.loader').css('display', 'none');
+        }, 500)
+        
+    }, 2000)
+
+)
